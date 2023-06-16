a19a4838570457aa7aad9da87c487dc9f4e143f2
SheikhULAB <tosibalaptop86@gmail.com>
2023-06-16 17:34:35 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    optimizeFonts: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+            },
+        ],
+        minimumCacheTTL: 1500000,
+    },
+};
 
 module.exports = nextConfig
